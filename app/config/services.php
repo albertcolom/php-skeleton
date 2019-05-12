4a18ddea52665235e9789fd671d00cdc31c12cf3
Albert Colom <skolom@gmail.com>
2019-05-12 10:35:48 +0200

    Update Di Autowire Wildcard repository

--- app/config/services.php
+++ app/config/services.php
@@ -3,5 +3,5 @@
 // More info: http://php-di.org/
 
 return [
-    'App\Domain\Repository\FooRepository' => DI\autowire('App\Infrastructure\Repository\InMemoryFooRepository')
+    'App\Domain\Repository\*Repository' => DI\create('App\Infrastructure\Repository\InMemory*Repository')
 ];
