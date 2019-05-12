22f5023e6fd0414ce8ccf8469d685e1c58e7eb1d
Albert Colom <skolom@gmail.com>
2019-05-12 10:34:10 +0200

    Update router config

--- app/config/router.php
+++ app/config/router.php
@@ -2,14 +2,16 @@
 
 // More info: https://github.com/nikic/FastRoute
 
+use App\Application\FooClass;
+
 return FastRoute\simpleDispatcher(function (FastRoute\RouteCollector $r) {
     // http://localhost:8000 | http://localhost:8000/
-    $r->addRoute('GET', '[/]', '\App\Application\FooClass');
+    $r->addRoute('GET', '[/]', FooClass::class);
 
     // http://localhost:8000/info | http://localhost:8000/info/
-    $r->addRoute('GET', '/info[/]', ['\App\Application\FooClass', 'info']);
+    $r->addRoute('GET', '/info[/]', [FooClass::class, 'info']);
 
     // http://localhost:8000/hello | http://localhost:8000/hello/
     // http://localhost:8000/hello/foo | http://localhost:8000/hello/foo/
-    $r->addRoute('GET', '/hello[/[{name}[/]]]', ['\App\Application\FooClass', 'hello']);
+    $r->addRoute('GET', '/hello[/[{name}[/]]]', [FooClass::class, 'hello']);
 });
